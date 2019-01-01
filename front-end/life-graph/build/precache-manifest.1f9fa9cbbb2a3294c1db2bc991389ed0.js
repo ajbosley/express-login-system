240adf570da235b758cf204130c9c2e1082c33bb
Aaron Bosley <aaron.bosley@consortiq.com>
2019-01-01 14:55:02 +0000

    Added react

---
+++ front-end/life-graph/build/precache-manifest.1f9fa9cbbb2a3294c1db2bc991389ed0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "4cc5472e0928c9467560",
+    "url": "/static/js/main.4cc5472e.chunk.js"
+  },
+  {
+    "revision": "0e493f63e1ac10c82d37",
+    "url": "/static/js/1.0e493f63.chunk.js"
+  },
+  {
+    "revision": "4cc5472e0928c9467560",
+    "url": "/static/css/main.739b2168.chunk.css"
+  },
+  {
+    "revision": "677cd4856c95a6e57e7f8e49d1b95815",
+    "url": "/index.html"
+  }
+];
